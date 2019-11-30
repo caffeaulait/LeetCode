90bee067dfc061cd792c2a64e08fd2e02c06b5e7
Yang Lu <yang.youngsoul@gmail.com>
2019-11-30 13:31:51 +0800

    10 regular expression match

---
+++ src/com/caffeaulait/_0010_RegularExpressionMatching.java
@@ -0,0 +1,69 @@
+package com.caffeaulait;
+
+public class _0010_RegularExpressionMatching {
+    /**
+     * #10
+     * Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*'.
+     *
+     * '.' Matches any single character.
+     * '*' Matches zero or more of the preceding element.
+     * The matching should cover the entire input string (not partial).
+     *
+     * Note:
+     *
+     * s could be empty and contains only lowercase letters a-z.
+     * p could be empty and contains only lowercase letters a-z, and characters like . or *.
+     * Example 1:
+     *
+     * Input:
+     * s = "aa"
+     * p = "a"
+     * Output: false
+     * Explanation: "a" does not match the entire string "aa".
+     * Example 2:
+     *
+     * Input:
+     * s = "aa"
+     * p = "a*"
+     * Output: true
+     * Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+     * Example 3:
+     *
+     * Input:
+     * s = "ab"
+     * p = ".*"
+     * Output: true
+     * Explanation: ".*" means "zero or more (*) of any character (.)".
+     * Example 4:
+     *
+     * Input:
+     * s = "aab"
+     * p = "c*a*b"
+     * Output: true
+     * Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore, it matches "aab".
+     * Example 5:
+     *
+     * Input:
+     * s = "mississippi"
+     * p = "mis*is*p*."
+     * Output: false
+     */
+
+    public boolean isMatch(String text, String pattern) {
+        //check empty
+        if (pattern.isEmpty()) return text.isEmpty();
+
+        //check first char
+        boolean first_match = (!text.isEmpty() &&
+                (pattern.charAt(0) == text.charAt(0) || pattern.charAt(0) == '.'));
+
+        //recursive case
+        if (pattern.length() >= 2 && pattern.charAt(1) == '*'){
+            //*match zero chars or >=1 chars
+            return (isMatch(text, pattern.substring(2)) ||
+                    (first_match && isMatch(text.substring(1), pattern)));
+        } else {
+            return first_match && isMatch(text.substring(1), pattern.substring(1));
+        }
+    }
+}
