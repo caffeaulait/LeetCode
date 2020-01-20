867466db113053e11e619291f2e4c8dcf4c9db50
Yang Lu <yang.youngsoul@gmail.com>
2020-01-20 23:51:06 +0800

    z

--- src/com/caffeaulait/_0003_LongestSubstringWithoutRepeatingCharacters.java
+++ src/com/caffeaulait/_0003_LongestSubstringWithoutRepeatingCharacters.java
@@ -68,17 +68,17 @@ public class _0003_LongestSubstringWithoutRepeatingCharacters {
 
     //optimized sliding window
     public int $$lengthOfLongestSubstring(String s){
-        int n = s.length();
-        int result = 0;
+        int max = 0;
         Map<Character,Integer> map = new HashMap<>();
 
-        for (int j = 0, i = 0; j < n; j++){
-            if (map.containsKey(s.charAt(j))){
-                i = Math.max(map.get(s.charAt(j)),i);
+        for (int i = 0,j=0;j<s.length();j++){
+            char c = s.charAt(j);
+            if (map.containsKey(c)){
+                i = Math.max(i,map.get(c)+1);
             }
-            result = Math.max(result, j - i + 1);
-            map.put(s.charAt(j), j + 1);
+            map.put(c,j);
+            max = Math.max(max,j-i+1);
         }
-        return result;
+        return max;
     }
 }
---
+++ src/com/caffeaulait/_0617_MergeTwoBinaryTrees.java
@@ -0,0 +1,30 @@
+package com.caffeaulait;
+
+public class _0617_MergeTwoBinaryTrees {
+    /**
+     * Given two binary trees and imagine that when you put one of them to cover the other, some nodes of the two trees
+     * are overlapped while the others are not.
+     *
+     * You need to merge them into a new binary tree. The merge rule is that if two nodes overlap, then sum node values
+     * up as the new value of the merged node. Otherwise, the NOT null node will be used as the node of new tree.
+     */
+
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        TreeNode root = null;
+        if (t1 == null && t2 == null) {
+            return null;
+        }
+        else if(t1==null){
+            root = t2;
+        }
+        else if(t2==null){
+            root = t1;
+        }
+        else {
+            root = new TreeNode(t1.val+t2.val);
+            root.left = mergeTrees(t1.left,t2.left);
+            root.right = mergeTrees(t1.right,t2.right);
+        }
+        return root;
+    }
+}
