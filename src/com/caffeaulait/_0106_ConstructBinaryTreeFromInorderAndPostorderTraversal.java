8e3280b7957197b2a5e830f54f6340a08afd6ba6
Yang Lu <yang.youngsoul@gmail.com>
2020-02-21 09:43:21 +0800

    .

---
+++ src/com/caffeaulait/_0106_ConstructBinaryTreeFromInorderAndPostorderTraversal.java
@@ -0,0 +1,40 @@
+package com.caffeaulait;
+
+import java.util.Arrays;
+
+public class _0106_ConstructBinaryTreeFromInorderAndPostorderTraversal {
+    /**
+     * Given inorder and postorder traversal of a tree, construct the binary tree.
+     *
+     * Note:
+     * You may assume that duplicates do not exist in the tree.
+     *
+     * For example, given
+     *
+     * inorder = [9,3,15,20,7]
+     * postorder = [9,15,7,20,3]
+     * Return the following binary tree:
+     *
+     *     3
+     *    / \
+     *   9  20
+     *     /  \
+     *    15   7
+     */
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        if (postorder.length<=0) return null;
+        int value = postorder[postorder.length-1];
+        int index = 0;
+        for (int i = 0; i < inorder.length; i++){
+            if (inorder[i] == value){
+                index = i;
+                break;
+            }
+        }
+        TreeNode root = new TreeNode(value);
+        root.left = buildTree(Arrays.copyOfRange(inorder,0,index), Arrays.copyOfRange(postorder,0, index));
+        root.right = buildTree(Arrays.copyOfRange(inorder,index+1,inorder.length), Arrays.copyOfRange(postorder,index, postorder.length-1));
+        return root;
+    }
+}
+
---
+++ src/com/caffeaulait/_0109_ConvertSortedListToBinarySearchTree.java
@@ -0,0 +1,51 @@
+package com.caffeaulait;
+
+public class _0109_ConvertSortedListToBinarySearchTree {
+    /**
+     * Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
+     *
+     * For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+     *
+     * Example:
+     *
+     * Given the sorted linked list: [-10,-3,0,5,9],
+     *
+     * One possible answer is: [0,-3,9,-10,null,5], which represents the following height balanced BST:
+     *
+     *       0
+     *      / \
+     *    -3   9
+     *    /   /
+     *  -10  5
+     */
+
+    public TreeNode sortedListToBST(ListNode head) {
+        if (head == null) return null;
+
+        ListNode mid = getMidNode(head);
+
+        TreeNode root = new TreeNode(mid.val);
+
+        if (head == mid) return root;
+
+        root.left = sortedListToBST(head);
+        root.right = sortedListToBST(mid.next);
+        return root;
+    }
+
+    private ListNode getMidNode(ListNode head){
+        ListNode prev = null;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast!=null && fast.next!=null){
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        if (prev!=null) prev.next = null;
+
+        return slow;
+    }
+}
