09d17cd4195a6077f0e095c948594b2a70018234
Yang Lu <yang.youngsoul@gmail.com>
2020-04-01 19:34:57 +0800

    lc 30 days challenge - day1

---
+++ src/com/caffeaulait/_0154_FindMinimumInRotatedSortedArrayII.java
@@ -0,0 +1,37 @@
+package com.caffeaulait;
+
+public class _0154_FindMinimumInRotatedSortedArrayII {
+    /**
+     * Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+     *
+     * (i.e.,  [0,1,2,4,5,6,7] might become  [4,5,6,7,0,1,2]).
+     *
+     * Find the minimum element.
+     *
+     * The array may contain duplicates.
+     *
+     * Example 1:
+     *
+     * Input: [1,3,5]
+     * Output: 1
+     * Example 2:
+     *
+     * Input: [2,2,2,0,1]
+     * Output: 0
+     * Note:
+     */
+    public int findMin(int[] nums) {
+        int lo = 0, hi = nums.length-1;
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+            if (nums[mid] < nums[hi]){
+                hi = mid;
+            }else if (nums[mid] > nums[hi]){
+                lo = mid + 1;
+            }else {
+                hi = hi - 1;
+            }
+        }
+        return nums[lo];
+    }
+}
---
+++ src/com/caffeaulait/_0173_BinarySearchTreeIterator.java
@@ -0,0 +1,81 @@
+package com.caffeaulait;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+public class _0173_BinarySearchTreeIterator {
+    /**
+     * Implement an iterator over a binary search tree (BST). Your iterator will be initialized with the root node of a BST.
+     *
+     * Calling next() will return the next smallest number in the BST.
+     *
+     *
+     *
+     * Example:
+     *
+     *
+     *
+     * BSTIterator iterator = new BSTIterator(root);
+     * iterator.next();    // return 3
+     * iterator.next();    // return 7
+     * iterator.hasNext(); // return true
+     * iterator.next();    // return 9
+     * iterator.hasNext(); // return true
+     * iterator.next();    // return 15
+     * iterator.hasNext(); // return true
+     * iterator.next();    // return 20
+     * iterator.hasNext(); // return false
+     */
+//    List<Integer> values = new ArrayList<>();
+//
+//    public _0173_BinarySearchTreeIterator(TreeNode root) {
+//        toList(root);
+//    }
+//
+//    /** @return the next smallest number */
+//    public int next() {
+//        return values.remove(0);
+//    }
+//
+//    /** @return whether we have a next smallest number */
+//    public boolean hasNext() {
+//        return (values.size()>0);
+//    }
+//
+//    public void toList(TreeNode root){
+//        if (root==null) return;
+//        toList(root.left);
+//        values.add(root.val);
+//        toList(root.right);
+//    }
+
+    private Stack<TreeNode> stack;
+    public _0173_BinarySearchTreeIterator(TreeNode root){
+        stack = new Stack<>();
+        TreeNode curr = root;
+        while ( curr != null) {
+            stack.push(curr);
+            curr = curr.left;
+        }
+    }
+
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+
+    public int next() {
+        TreeNode node = stack.pop();
+        TreeNode curr = node;
+        if (curr.right!=null){
+            curr = curr.right;
+            while (curr!=null){
+                stack.push(curr);
+                curr = curr.left;
+            }
+        }
+        return node.val;
+    }
+
+}
