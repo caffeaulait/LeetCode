f20d771e0dcac5e63c8c2312e58d2dfa8e523f42
Yang Lu <yang.youngsoul@gmail.com>
2020-04-10 18:49:59 +0800

    lc 30 days challenge -day 10

---
+++ src/com/caffeaulait/_0117_PopulatingNextRightPointersInEachNodeII.java
@@ -0,0 +1,32 @@
+package com.caffeaulait;
+
+public class _0117_PopulatingNextRightPointersInEachNodeII {
+    /**
+     *Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+     *
+     * Initially, all next pointers are set to NULL.
+     */
+    public Node connect(Node root) {
+
+    }
+
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    };
+}
