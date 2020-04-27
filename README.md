e70d6314ac311c023cc543f89615fe79a1e91e39
Yang Lu <yang.youngsoul@gmail.com>
2020-04-27 16:40:04 +0800

    lc 30 days challenge -day 27

--- README.md
+++ README.md
@@ -8,5 +8,5 @@
 **Weekly Contest 184** &emsp; solved: q1, q2, q3, q4  
 **Weekly Contest 185** &emsp; solved: q2, q2  
 **BiWeeklyContest 24** &emsp; solved: q1, q2, q3  
-**LCCN Spring Contest** &emsp; solved: q1, q2
+**LCCN Spring Contest** &emsp; solved: q1, q2   
 **Weekly Contest 186** &emsp; solved: q1 
---
+++ src/com/caffeaulait/_0221_MaximalSquare.java
@@ -0,0 +1,33 @@
+package com.caffeaulait;
+
+public class _0221_MaximalSquare {
+    /**
+     * Given a 2D binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its area.
+     *
+     * Example:
+     *
+     * Input:
+     *
+     * 1 0 1 0 0
+     * 1 0 1 1 1
+     * 1 1 1 1 1
+     * 1 0 0 1 0
+     *
+     * Output: 4
+     */
+
+    public int maximalSquare(char[][] matrix) {
+        if (matrix.length == 0) return 0;
+        int m = matrix.length, n = matrix[0].length, result = 0;
+        int[][] dp = new int[m+1][n+1];
+        for (int i = 1; i<= m; i++){
+            for (int j = 1; j <= n; j++){
+                if (matrix[i-1][j-1] == '1'){
+                    dp[i][j] = Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1])) + 1;
+                    result = Math.max(result, dp[i][j]);
+                }
+            }
+        }
+        return result * result;
+    }
+}
