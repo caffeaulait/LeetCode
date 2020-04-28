a755b1e4a2b3d36db966884d748da0afb947feea
Yang Lu <yang.youngsoul@gmail.com>
2020-04-28 21:25:19 +0800

    lc 30 days challenge -day 28

--- src/com/caffeaulait/nowcoder_26.java
+++ src/com/caffeaulait/nowcoder_26.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-public class nowcoder_26 {
+public class    nowcoder_26 {
     /**
      * 把只包含质因子2、3和5的数称作丑数（Ugly Number）。例如6、8都是丑数，但14不是，因为它包含质因子7。
      * 习惯上我们把1当做是第一个丑数。求按从小到大的顺序的第N个丑数。
