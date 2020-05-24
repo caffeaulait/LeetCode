87cfe7b5a53e00edd8f1f6a12637dbb9b7c3edd9
Yang Lu <yang.youngsoul@gmail.com>
2020-05-24 15:45:00 +0800

    lc May challenge -day 24

---
+++ src/com/caffeaulait/MaximumProductOfThreeNumbers.java
@@ -0,0 +1,46 @@
+package com.caffeaulait;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class MaximumProductOfThreeNumbers {
+    /**
+     * Given an integer array, find three numbers whose product is maximum and output the maximum product.
+     *
+     * Example 1:
+     *
+     * Input: [1,2,3]
+     * Output: 6
+     *
+     *
+     * Example 2:
+     *
+     * Input: [1,2,3,4]
+     * Output: 24
+     *
+     */
+
+    public int maximumProduct(int[] nums) {
+        Queue<Integer> pq = new PriorityQueue<>();
+        Queue<Integer> nq = new PriorityQueue<>((a,b) -> (b-a));
+        for (int num : nums){
+            pq.offer(num);
+            nq.offer(num);
+            if (pq.size() > 3) pq.poll();
+            if (nq.size() > 2) nq.poll();
+        }
+        int pos = 1;
+        int max = 0;
+        while (!pq.isEmpty()){
+            max = pq.poll();
+            pos *= max;
+        }
+        int neg = max;
+        int min = 0;
+        while (!nq.isEmpty()){
+            min = nq.poll();
+            neg *= min;
+        }
+        return Math.max(pos, neg);
+    }
+}
