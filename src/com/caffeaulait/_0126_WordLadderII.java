dd92f23e294deb40baf7aca64bbb54a94065295e
Yang Lu <yang.youngsoul@gmail.com>
2020-08-13 13:17:34 +0800

    .

--- src/com/caffeaulait/_0126_WordLadderII.java
+++ src/com/caffeaulait/_0126_WordLadderII.java
@@ -1,7 +1,6 @@
 package com.caffeaulait;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class _0126_WordLadderII {
     /**
@@ -41,7 +40,74 @@ public class _0126_WordLadderII {
      */
 
     public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+        Set<String> dict = new HashSet<>(wordList);
+        dict.add(beginWord);
         List<List<String>> res = new ArrayList<>();
-        if (!wordList.contains(endWord)) return res;
+        Map<String, List<String>> neighboursMap = new HashMap<>();
+        Map<String, Integer> distance = new HashMap<>();
+        bfs(beginWord, endWord, dict, neighboursMap, distance);
+        dfs(beginWord, endWord, neighboursMap, distance, new ArrayList<>(),
+                res);
+        return res;
+    }
+
+    private void bfs(String start, String end, Set<String> dict, Map<String,
+            List<String>> neighboursMap, Map<String, Integer> distance){
+        for (String s : dict){
+            neighboursMap.put(s, new ArrayList<>());
+        }
+        Queue<String> q = new LinkedList<>();
+        q.offer(start);
+        distance.put(start, 0);
+
+        while (!q.isEmpty()){
+            int size = q.size();
+            boolean found = false;
+            for (int i = 0; i < size; i++){
+                String curr = q.poll();
+                int currDist = distance.get(curr);
+                List<String> neighbours = getNeighbours(curr, dict);
+                for (String neighbour : neighbours) {
+                    neighboursMap.get(curr).add(neighbour);
+                    if (!distance.containsKey(neighbour)) {
+                        distance.put(neighbour, currDist + 1);
+                        if (end.equals(neighbour)) found = true;
+                        else q.offer(neighbour);
+                    }
+                }
+            }
+            if (found) break;
+        }
+    }
+
+    private List<String> getNeighbours(String str, Set<String> dict){
+        List<String> res = new ArrayList<>();
+        char[] chars = str.toCharArray();
+        for (int i = 0; i < chars.length; i++){
+            for (char c = 'a'; c <= 'z'; c++){
+                if (chars[i] == c) continue;;
+                char temp = chars[i];
+                chars[i] = c;
+                if (dict.contains(String.valueOf(chars))) res.add(String.valueOf(chars));
+                chars[i] = temp;
+            }
+        }
+        return res;
+    }
+
+    private void dfs(String curr, String end, Map<String,
+            List<String>> neighboursMap, Map<String, Integer> distance,
+                     List<String> solution, List<List<String>> res) {
+        solution.add(curr);
+        if (end.equals(curr)){
+            res.add(new ArrayList<>(solution));
+        }else{
+            for (String neighbour : neighboursMap.get(curr)) {
+                if (distance.get(curr) + 1 == distance.get(neighbour)){
+                    dfs(neighbour, end, neighboursMap, distance, solution, res);
+                }
+            }
+        }
+        solution.remove(solution.size()-1);
     }
 }
