df00ee718bb54f4c8d6a9b29f81a9857453eb0d2
Yang Lu <yang.youngsoul@gmail.com>
2020-12-19 09:43:42 +0800

    .

--- src/com/caffeaulait/_0166_FractionToRecurringDecimal.java
+++ src/com/caffeaulait/_0166_FractionToRecurringDecimal.java
@@ -1,5 +1,8 @@
 package com.caffeaulait;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class _0166_FractionToRecurringDecimal {
     /**
      * Given two integers representing the numerator and denominator of a fraction, return the fraction in string format.
@@ -35,6 +38,30 @@ public class _0166_FractionToRecurringDecimal {
      */
 
     public String fractionToDecimal(int numerator, int denominator) {
-        return null;
+        if (numerator == 0) return "0";
+        StringBuilder sb = new StringBuilder();
+        sb.append( (numerator > 0) ^ (denominator > 0) ? "-" : "");
+        long num = Math.abs((long) numerator);
+        long den = Math.abs((long) denominator);
+        sb.append(num / den);
+        num %= den;
+        if (num == 0) return sb.toString();
+        sb.append(".");
+        Map<Long, Integer> map = new HashMap<>();
+        map.put(num, sb.length());
+        while (num != 0) {
+            num *= 10;
+            sb.append(num /den);
+            num %= den;
+            if (map.containsKey(num)){
+                int index = map.get(num);
+                sb.insert(index, "(");
+                sb.append(")");
+                break;
+            }else {
+                map.put(num, sb.length());
+            }
+        }
+        return sb.toString();
     }
 }
---
+++ src/com/caffeaulait/_0168_ExcelSheetColumnTitle.java
@@ -0,0 +1,43 @@
+package com.caffeaulait;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class _0168_ExcelSheetColumnTitle {
+    /**
+     * Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+     *
+     * For example:
+     *
+     *     1 -> A
+     *     2 -> B
+     *     3 -> C
+     *     ...
+     *     26 -> Z
+     *     27 -> AA
+     *     28 -> AB
+     *     ...
+     * Example 1:
+     *
+     * Input: 1
+     * Output: "A"
+     * Example 2:
+     *
+     * Input: 28
+     * Output: "AB"
+     * Example 3:
+     *
+     * Input: 701
+     * Output: "ZY"
+     */
+
+    public String convertToTitle(int n) {
+        StringBuilder res = new StringBuilder();
+        while ( n > 0) {
+            n--;
+            res.insert(0, (char) ('A' + n % 26));
+            n /= 26;
+        }
+        return res.toString();
+    }
+}
