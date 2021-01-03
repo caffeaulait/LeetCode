4fefbb42155ceb1eb7b99c44d0a39cc2bd69e552
Yang Lu <yang.youngsoul@gmail.com>
2021-01-03 11:19:55 +0800

    .

---
+++ src/com/caffeaulait/BiWeeklyContest42.java
@@ -0,0 +1,65 @@
+package com.caffeaulait;
+
+import java.util.*;
+
+public class BiWeeklyContest42 {
+    /**
+     * q1
+     * q2
+     * q3
+     * q4
+     */
+
+    public int countStudents(int[] students, int[] sandwiches) {
+        List<Integer> list = new LinkedList<>(toList(students));
+        List<Integer> food = new LinkedList<>(toList(sandwiches));
+        while (list.size() > 0) {
+            if (!list.get(0).equals(food.get(0))) {
+                if (isSame(list)) {
+                    break;
+                }else {
+                    int el = list.remove(0);
+                    list.add(el);
+                }
+            }else {
+                list.remove(0);
+                food.remove(0);
+            }
+        }
+        return list.size();
+    }
+
+    private List<Integer> toList(int[] students) {
+        List<Integer> list = new ArrayList<>();
+        for (int el : students) list.add(el);
+        return list;
+    }
+
+    private boolean isSame(List<Integer> list) {
+        if (list.size() == 0) return true;
+        int el = list.get(0);
+        for (int i = 1; i < list.size(); i++) {
+            if (el != list.get(i)) return false;
+        }
+        return true;
+    }
+
+    public double averageWaitingTime(int[][] customers) {
+        int count = customers.length;
+        double waitTime = 0;
+        int total = customers[0][0];
+        for (int[] customer : customers) {
+            if (total >= customer[0]) {
+                total += customer[1];
+            }else {
+                total = customer[0] + customer[1];
+            }
+            waitTime += (total - customer[0]);
+        }
+        return waitTime / (count * 1.0);
+    }
+
+    public String maximumBinaryString(String binary) {
+        return null;
+    }
+}
