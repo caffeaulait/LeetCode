24002d979dabb415beece16146d0338c3c394b13
Yang Lu <yang.youngsoul@gmail.com>
2021-11-28 11:18:56 +0800

    .

--- src/com/caffeaulait/miscellaneous/ListNode.java
+++ src/com/caffeaulait/miscellaneous/ListNode.java
@@ -3,7 +3,7 @@ package com.caffeaulait.miscellaneous;
 public class ListNode {
     public int val;
     public ListNode next;
-    ListNode(int x){
+    public ListNode(int x){
         val = x;
     }
 }
