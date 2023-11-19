d577b3bd8097347b9b0e70f2a0e659a9a54ef842
Yang Lu <yang.youngsoul@gmail.com>
2023-11-19 22:56:18 +0800

    Update Pair.java
    .

--- src/com/caffeaulait/struct/Pair.java
+++ src/com/caffeaulait/struct/Pair.java
@@ -1,2 +1,13 @@
-package com.caffeaulait.struct;public class Pair {
+package com.caffeaulait.struct;
+
+public class Pair<T, E> {
+
+    private T first;
+
+    private E second;
+
+    public Pair(T first, E second) {
+        this.first = first;
+        this.second = second;
+    }
 }
